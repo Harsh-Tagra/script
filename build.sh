3b9a17508f1850a58127e876b572a50a6e9c7e23
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-05-26 19:03:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ export BUILD_USERNAME=harsh
 export BUILD_HOSTNAME=crave
 export RISING_MAINTAINER=Harsh Tagra
 export WITH_GMS = true
+export TARGET_CORE_GMS = true
 export TARGET_CORE_GMS_EXTRAS =true
 export TARGET_DEFAULT_PIXEL_LAUNCHER = true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
