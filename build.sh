99a9f82c10b14d7dbe776d9704171444b4b2aeed
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-05-28 17:34:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
-
+sudo apt install git-lfs
+git lfs install
+repo init   -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+rm -rf vendor/gms
+rm -rf .repo/projects/vendor/gms.git
+rm -rf .repo/project-objects/*/android_vendor_gms.git 
+rm -rf .repo/local_manifests/ 
 # Clone RisingTechOSS 14
-repo init --depth 1 -u https://github.com/RisingTechOSS/android.git -b fourteen --git-lfs
+repo init  --depth 1 -u https://github.com/RisingTechOSS/android.git -b fourteen --git-lfs
 # Clone local_manifests repository
-git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b main .repo/local_manifests
 if [ ! 0 == 0 ]
- then   curl -o .repo/local_manifests https://github.com/krishnaspeace/local_manifests.git
+ then   curl -o .repo/local_manifests https://github.com/Harsh-Tagra/local_manifests.git
  fi
 # repo sync
 /opt/crave/resync.sh
@@ -14,12 +21,11 @@ git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_open
 # Set up build environment
 export BUILD_USERNAME=harsh
 export BUILD_HOSTNAME=crave
-export RISING_MAINTAINER=Harsh Tagra
+export RISING_MAINTAINER=harsh
 export WITH_GMS=true
 export TARGET_CORE_GMS=true
 export TARGET_CORE_GMS_EXTRAS=true
 export TARGET_DEFAULT_PIXEL_LAUNCHER=true
-
 source build/envsetup.sh
 # build
 riseup ysl userdebug
