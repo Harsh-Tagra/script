a51656b6b55f7dc4f4815f41f1b5c1554bdd9f74
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-05-29 12:29:43 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ rm -rf .repo/local_manifests/
 # Clone RisingTechOSS 14
 repo init  --depth 1 -u https://github.com/RisingTechOSS/android.git -b fourteen --git-lfs
 # Clone local_manifests repository
-git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b main .repo/local_manifests
 if [ ! 0 == 0 ]
- then   curl -o .repo/local_manifests https://github.com/krishnaspeace/local_manifests.git
+ then   curl -o .repo/local_manifests https://github.com/Harsh-Tagra/local_manifests.git
  fi
 # repo sync
 /opt/crave/resync.sh
@@ -24,6 +24,12 @@ export BUILD_HOSTNAME=crave
 export RISING_MAINTAINER=harsh
 export WITH_GMS=true
 export TARGET_CORE_GMS=true
+export PRODUCT_PACKAGES += \
+CalculatorGooglePrebuilt\
+CalendarGooglePrebuilt \
+Google\
+Velvet\
+
 source build/envsetup.sh
 # build
 riseup ysl userdebug
