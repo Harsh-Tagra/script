1b8840c09b13caf1ee640b693a0209b8680734b5
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-05-31 14:29:30 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,18 +3,9 @@
 sudo apt install -y git-lfs
 git lfs install
 
-# Initialize repo with RisingTechOSS android repository
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
-
-# Remove existing gms vendor directory and related repo projects
-rm -rf vendor/gms
-rm -rf .repo/projects/vendor/gms.git
-rm -rf .repo/project-objects/*/android_vendor_gms.git
-rm -rf .repo/local_manifests/
 
 # Clone RisingTechOSS 14 repository with depth 1
-repo init --depth=1 -u https://github.com/RisingTechOSS/android.git -b fourteen --git-lfs
+repo init --depth=1 -u https://github.com/ProjectBlaze/manifest -b 14-QPR2
 
 # Clone local_manifests repository
 git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b main .repo/local_manifests
@@ -36,7 +27,7 @@ git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_open
 # Export environment variables for the build
 export BUILD_USERNAME=harsh
 export BUILD_HOSTNAME=crave
-export RISING_MAINTAINER=Harsh-Tagra
+export BLAZE_MAINTAINER=Harsh-Tagra
 export WITH_GMS=true
 export TARGET_CORE_GMS=true
 
