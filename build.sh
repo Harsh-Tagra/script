6da07a92472513534f6f53f8d4adc479297e39db
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-06-04 18:52:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,52 +1,30 @@
-#!/bin/bash
-# Install git-lfs
-sudo apt install -y git-lfs
+sudo apt install git-lfs
 git lfs install
-
-# Initialize repo with RisingTechOSS android repository
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+repo init   -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
-
-# Remove existing gms vendor directory and related repo projects
 rm -rf vendor/gms
 rm -rf .repo/projects/vendor/gms.git
-rm -rf .repo/project-objects/*/android_vendor_gms.git
-rm -rf .repo/local_manifests/
-
-# Clone RisingTechOSS 14 repository with depth 1
-repo init --depth=1 -u https://github.com/RisingTechOSS/android.git -b fourteen --git-lfs
-
+rm -rf .repo/project-objects/*/android_vendor_gms.git 
+rm -rf .repo/local_manifests/ 
+# Clone RisingTechOSS 14
+repo init  --depth 1 -u https://github.com/RisingTechOSS/android.git -b fourteen --git-lfs
 # Clone local_manifests repository
-git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b main .repo/local_manifests
-
-if [! 0 == 0 ]; then
-    curl -o .repo/local_manifests https://github.com/Harsh-Tagra/local_manifests.git
-fi
-
-# Sync repo
+git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b main .repo/local_manifests
+if [ ! 0 == 0 ]
+ then   curl -o .repo/local_manifests https://github.com/krishnaspeace/local_manifests.git
+ fi
+# repo sync
 /opt/crave/resync.sh
-
-# Fixing fingerprint issue by cloning the required repository
-rm -rf vendor/fingerprint/opensource/interfaces
+# Fixing fingerprint
+rm -rf vendor/fingerprint/opensurce/interfaces
 git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_opensource_interfaces vendor/fingerprint/opensource/interfaces
-
-# Set up build environment by cloning required repository
-
-
-# Export environment variables for the build
+# Set up build environment
 export BUILD_USERNAME=harsh
 export BUILD_HOSTNAME=crave
-export RISING_MAINTAINER=Harsh-Tagra
+export RISING_MAINTAINER=harsh
 export WITH_GMS=true
 export TARGET_CORE_GMS=true
-
-
-
-# Source the build environment setup script
 source build/envsetup.sh
-
-# Build the environment for ysl userdebug
+# build
 riseup ysl userdebug
-
-# Execute the build command
 rise b
