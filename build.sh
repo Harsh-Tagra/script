365454523060bd877c4e97f185c4be20c117ba5d
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-06-10 05:29:22 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 sudo apt install git-lfs
 git lfs install
-repo init   -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
-rm -rf vendor/gms
-rm -rf .repo/projects/vendor/gms.git
-rm -rf .repo/project-objects/*/android_vendor_gms.git 
-rm -rf .repo/local_manifests/ 
-# Clone RisingTechOSS 14
-repo init  --depth 1 -u https://github.com/RisingTechOSS/android.git -b fourteen --git-lfs
-# Clone local_manifests repository
+repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
+repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j20
+
 git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b main .repo/local_manifests
 if [ ! 0 == 0 ]
  then   curl -o .repo/local_manifests https://github.com/Harsh-Tagra/local_manifests.git
@@ -21,9 +15,8 @@ git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_open
 # Set up build environment
 export BUILD_USERNAME=harsh
 export BUILD_HOSTNAME=crave
-export RISING_MAINTAINER=harsh
-export WITH_GMS=true
-export TARGET_CORE_GMS=true
+export SAKURA_BUILD_TYPE=gapps
+export TARGET_SUPPORTS_64_BIT_APPS=true
 source build/envsetup.sh
 # build
 riseup ysl userdebug
