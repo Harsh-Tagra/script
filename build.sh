0a319dd0baba7672ae35b01ab77217b06606f529
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-06-10 05:30:36 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 sudo apt install git-lfs
 git lfs install
 repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j20
 
 git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b main .repo/local_manifests
 if [ ! 0 == 0 ]
  then   curl -o .repo/local_manifests https://github.com/Harsh-Tagra/local_manifests.git
  fi
-# repo sync
-curl https://raw.githubusercontent.com/sounddrill31/docker-images/04449990912b9d7ee594193883740037f0ac80a7/aosp/common/resync.sh | bash
+repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j25
+
 # Fixing fingerprint
 rm -rf vendor/fingerprint/opensurce/interfaces
 git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_opensource_interfaces vendor/fingerprint/opensource/interfaces
