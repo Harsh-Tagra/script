2e55b7eb2218473b64c6a823d1fc6674621f88ce
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-06-11 17:50:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ export TARGET_SUPPORTS_64_BIT_APPS=true
 export TARGET_BOOT_ANIMATION_RES= 720
 export SAKURA_MAINTAINER=Harsh-Tagra
 source build/envsetup.sh
-
+lunch lineage_ysl-ap1a-userdebug
 mka target-files-package otatools
 /opt/crave/crave_sign.sh
 # build
