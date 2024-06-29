1ba5d35099212c93f276e55fa2e3dab8a62cccbe
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-06-29 09:43:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,6 +16,6 @@ export TARGET_SUPPORTS_64_BIT_APPS=true
 export TARGET_BOOT_ANIMATION_RES=720
 export SAKURA_MAINTAINER=Harsh-Tagra
 source build/envsetup.sh
-lunch lineage_ysl-ap1a-userdebug
+lunch lineage_ysl-ap2a-userdebug
 mka bacon
 
