a4286ccfe4976136e0db855ea6fda57e46e652b4
Harsh-Tagra <harshtagra905@gmail.com>
2024-08-09 16:33:41 -0400

    rise init

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 sudo apt install git-lfs
 git lfs install
-repo init --depth=1 -u  https://github.com/ProjectSakura/android.git -b 14 --git-lfs
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 rm -rf .repo/local_manifests
-git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b ProjectSakura .repo/local_manifests
+git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b main .repo/local_manifests
 /opt/crave/resync.sh
 # Fixing fingerprint
  sudo rm -rf vendor/fingerprint/opensource/interfaces
@@ -11,8 +11,6 @@ git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_open
 # Set up build environment
 export BUILD_USERNAME=harsh
 export BUILD_HOSTNAME=crave
-export SAKURA_BUILD_TYPE=gapps
 source build/envsetup.sh
-brunch lineage_ysl-ap2a-userdebug
-rm -rf vendor/sakura-priv
-
+riseup ysl UNOFFICIAL
+rise b
