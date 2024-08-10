76366735fc3604502445e5fdb6b58c03cab9338e
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-08-10 04:05:10 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ sudo apt install git-lfs
 git lfs install
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 rm -rf .repo/local_manifests
-git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b rise .repo/local_manifests
 /opt/crave/resync.sh
 # Fixing fingerprint
  sudo rm -rf vendor/fingerprint/opensource/interfaces
@@ -11,7 +11,6 @@ git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_open
 # Set up build environment
 export BUILD_USERNAME=harsh
 export BUILD_HOSTNAME=crave
-export RISING_BUILDTYPE="UNOFFICIAL"
 source build/envsetup.sh
 riseup ysl userdebug
 rise sb
