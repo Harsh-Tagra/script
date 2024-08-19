24e253ad459d391268617ee4e5598c739768b39c
Harsh-Tagra <harshtagra905@gmail.com>
2024-08-19 14:41:13 -0400

    crdroid init

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 sudo apt install git-lfs
 git lfs install
-repo init --depth=1 -u  https://github.com/ProjectSakura/android.git -b 14 --git-lfs
+repo init --depth=1 -u  https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
 rm -rf .repo/local_manifests
-git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b ProjectSakura .repo/local_manifests
+git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b crdroid .repo/local_manifests
 /opt/crave/resync.sh
 # Fixing fingerprint
  sudo rm -rf vendor/fingerprint/opensource/interfaces
@@ -11,8 +11,8 @@ git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_open
 # Set up build environment
 export BUILD_USERNAME=harsh
 export BUILD_HOSTNAME=crave
-export SAKURA_BUILD_TYPE=gapps
+
 source build/envsetup.sh
 brunch lineage_ysl-ap2a-userdebug
-rm -rf vendor/sakura-priv
+
 
