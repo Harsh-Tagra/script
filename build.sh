bbdb2ff080dfc4bf5d081d79613d8cec3dfaf9fc
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-09-06 07:54:49 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,10 +9,13 @@ git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b crdroi
 
 git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_opensource_interfaces vendor/fingerprint/opensource/interfaces
 # Set up build environment
+git clone https://github.com/Harsh-Tagra/rom-keys.git /tmp/rom-keys
+sudo mv /tmp/rom-keys/crdroid-priv/ /tmp/src/android/vendor/lineage-priv
+sudo rm -rf /tmp/rom-keys
 export BUILD_USERNAME=harsh
 export BUILD_HOSTNAME=crave
 
 source build/envsetup.sh
-brunch  ysl userdebug
+brunch  ysl userdebug && rm -rf vendor/lineage-priv
 
 
