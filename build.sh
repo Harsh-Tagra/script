d129b629189b5453bbd2db0822289a93bc8982c1
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-09-19 09:31:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b rise .
 git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_opensource_interfaces vendor/fingerprint/opensource/interfaces
 # Set up build environment
 sudo rm -rf packages/apps/Updater/app/src/main/res/values/strings.xml
-wget https://raw.githubusercontent.com/Harsh-Tagra/script/refs/heads/rise/strings.xml -P packages/apps/Updater/app/src/main/res/values/
+wget https://raw.githubusercontent.com/Harsh-Tagra/script/rise/strings.xml -P packages/apps/Updater/app/src/main/res/values/
 sudo rm -rf packages/apps/crDroidSettings/res/values/cr_arrays.xml
-
+wget https://raw.githubusercontent.com/Harsh-Tagra/script/rise/cr_arrays.xml -P packages/apps/crDroidSettings/res/values/
 git clone https://github.com/Harsh-Tagra/rom-keys.git /tmp/rom-keys
 sudo mv /tmp/rom-keys/rise-priv/ /tmp/src/android/vendor/lineage-priv
 sudo rm -rf /tmp/rom-keys
