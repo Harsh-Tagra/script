1ae9bf0ebd06bd366f98883cabdcf0bf5a4a22c0
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-09-19 14:52:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,14 +3,12 @@ git lfs install
 rm -rf .repo/local_manifests
 repo init -u https://github.com/ProjectBlaze/manifest -b 15 --git-lfs
 
-git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Harsh-Tagra/local_manifests.git --depth 1 -b test .repo/local_manifests
  /opt/crave/resync.sh
 rm -rf vendor/fingerprint/opensurce/interfaces
 git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_opensource_interfaces vendor/fingerprint/opensource/interfaces
 export BUILD_USERNAME=harsh
 export BUILD_HOSTNAME=crave
-export BLAZE_MAINTAINER=Harsh-Tagra
-export WITH_GMS=false
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 source build/envsetup.sh
 lunch lineage_ysl-ap3a-userdebug
